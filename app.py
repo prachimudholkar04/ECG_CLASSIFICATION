bc879227aa703c97f9a5a444e1344282d85dd6ed
Prachi mudholkar <47937201+prachimudholkar04@users.noreply.github.com>
2023-08-22 00:56:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.image(image, use_column_width=True)
 patient_name = st.text_input(label="Enter the name of Patient", value="")
 
 ECG_data = st.file_uploader(label="Upload the patient's ECG Data")
-
+label_2_name = {0:'Normal', 1:'Super Ventricular', 2: 'Ventricular' , 3:'Fusion Beats', 4: 'Unknown'}
 if ECG_data is not None:
     st.write(f"Patient Name: {patient_name} has uploaded the ECG data in file {ECG_data.name}")
     data = pd.read_csv(ECG_data,header=None, delimiter="\t")
@@ -22,4 +22,4 @@ if ECG_data is not None:
         reshape_ecg_data = data.reshape(1, data.shape[1], 1)
         model = load_model('baseline_1Dcnn_mitbih.h5')
         prediction = np.argmax(model.predict(reshape_ecg_data))
-        print(prediction)
+        st.write(f"ECG Prediction: {prediction}")
